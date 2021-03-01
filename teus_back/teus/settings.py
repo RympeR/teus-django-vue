e7d64b857cf5d8b5af0526ec40b2b286b7086ecd
george <georg.rashkov@gmail.com>
2021-03-01 18:59:12 +0300

    after restore api fixes

--- teus_back/teus/settings.py
+++ teus_back/teus/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'teus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'teus',
-        'USER': 'teus_dev',
-        'PASSWORD': 'teus_dev',
+        'NAME': 'teos',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
         'HOST': 'localhost',
         'PORT': '5432',
     }

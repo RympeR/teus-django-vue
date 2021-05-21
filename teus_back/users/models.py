a9b5803caa02df5e15c395270a4875b9098c42c7
George Rashkov <georg.rashkov@gmail.com>
2021-05-21 17:18:30 +0300

    image model update

--- teus_back/users/models.py
+++ teus_back/users/models.py
@@ -9,6 +9,8 @@ import datetime
 # Create your models here.
 import string    
 import random
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class User(models.Model):
     phone = models.CharField(verbose_name='Телефон',
@@ -18,8 +20,11 @@ class User(models.Model):
         'Фамилия', max_length=255, null=True, blank=True)
     company = models.CharField(
         'Компания', max_length=255, null=True, blank=True)
-    image = models.ImageField(
-        verbose_name='Фото профиля', null=True, blank=True)
+
+    image = ProcessedImageField(verbose_name='Фото профиля', upload_to='avatars',
+                                           processors=[ResizeToFill(500, 500)],
+                                           options={'quality': 100},
+                                           null=True, blank=True)
     push_on = models.BooleanField(default=False)
     token = models.CharField(max_length=255, null=True, blank=True)
     is_admin = models.BooleanField(default=False)
